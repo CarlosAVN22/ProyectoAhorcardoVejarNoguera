37de22e96afda707d8e049ad2bea8b76707712ba
CarlosAVN22 <carlosavejar222@gmail.com>
2025-04-06 16:46:07 -0600

    Cambio echo con el form6 para registratse

--- ProyectoAhorcardoVejarNoguera/Form3.Designer.cs
+++ ProyectoAhorcardoVejarNoguera/Form3.Designer.cs
@@ -32,6 +32,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.btnSalir = new System.Windows.Forms.Button();
             this.btnInicioSesion = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRegistrarse = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -81,12 +83,35 @@
             this.btnInicioSesion.UseVisualStyleBackColor = false;
             this.btnInicioSesion.Click += new System.EventHandler(this.btnInicioSesion_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(537, 181);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(30);
+            this.label3.Size = new System.Drawing.Size(151, 80);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Registrarse";
+            // 
+            // btnRegistrarse
+            // 
+            this.btnRegistrarse.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnRegistrarse.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnRegistrarse.Location = new System.Drawing.Point(574, 278);
+            this.btnRegistrarse.Name = "btnRegistrarse";
+            this.btnRegistrarse.Size = new System.Drawing.Size(87, 55);
+            this.btnRegistrarse.TabIndex = 9;
+            this.btnRegistrarse.Text = "Entrar";
+            this.btnRegistrarse.UseVisualStyleBackColor = false;
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::ProyectoAhorcardoVejarNoguera.Properties.Resources.ae508467_2673_43d5_9fb9_53570ac3a51d;
             this.ClientSize = new System.Drawing.Size(907, 519);
+            this.Controls.Add(this.btnRegistrarse);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnInicioSesion);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.label2);
@@ -108,5 +133,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.Button btnInicioSesion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRegistrarse;
     }
 }
